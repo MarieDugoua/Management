0fe10b0da8cd9f6d11b07eb03170132a35f71c26
MarieDugoua <mariedugoua@gmail.com>
2023-12-08 14:20:47 +0100

    refacto book.kt

--- src/main/kotlin/com/BookManagement/Management/domain/model/Book.kt
+++ src/main/kotlin/com/BookManagement/Management/domain/model/Book.kt
@@ -2,7 +2,7 @@ package com.BookManagement.Management.domain.model
 
 data class Book(val name: String, val author: String) {
     init {
-        require(name.isNotBlank()) { "com.BookManagement.Management.domain.model.Book name cannot be blank" }
-        require(author.isNotBlank()) { "com.BookManagement.Management.domain.model.Book author cannot be blank" }
+        require(name.isNotBlank()) { "Book name cannot be blank" }
+        require(author.isNotBlank()) { "Book author cannot be blank" }
     }
 }
